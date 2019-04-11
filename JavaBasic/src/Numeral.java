5c4d655fa36bddda64e1aaf33a712c6b72c408f0
daelonge <dleogus0569@naver.com>
2019-04-12 00:07:44 +0900

    1

--- JavaBasic/src/Numeral.java
+++ JavaBasic/src/Numeral.java
@@ -3,32 +3,34 @@ import java.util.*;
 public class Numeral {
 
 	public static void main(String[] args) {
-		int n = 8;
-		int x = 816;
+		int n = 14;
+		int x = 154;
 		int y = 0;
 		int i = 1;
 		int result = 0;
-		/*System.out.println(y);
-		x = x/n;
-		y = x%n;
-		x = x/n;
-		System.out.println(y);
-		System.out.println(x);
-		x = x*100;
-		*/
-		while(x>n) {
-			y = x/n; // 142/9=15
-			x = x%n; //142%9=7
-			x = x*i; //7*1
-			i = i*10;// i = 10
-			System.out.println(x);
+		int add = 0;
+		String character = "0123456789abcdef";
+		char cha = 'a';
+		
+		while(x>=n) {
+			y = x/n; // 몫
+			x = x%n; // 나머지
+			if( x>10 && x<n) {
+				cha = character.charAt(n);
+			}
+			else{
+				x = x*i; // 나머지 곱하기 자릿수
+			}
+			i = i*10;// 자릿수
 			result += x; //result = 7, 67
 			x = y; //몫을 다시한번 n으로 나누어주어야하니 x에 y값 대입
 		}
 		y = y*i;//최종 몫*i = 100
-		System.out.println(y);
 		result = result + y; //100*67
-		System.out.println(result);
+		if( cha == 'a' || cha == 'b' || cha == 'c' || cha == 'd' || cha == 'e' || cha == 'f') {
+			System.out.println(result + cha);
+		}
+		else System.out.println(result);
 	}
 
 }
--- JavaBasic/src/NumeralSystem.java
+++ JavaBasic/src/NumeralSystem.java
@@ -9,6 +9,11 @@ public class NumeralSystem {
 		int sum = 0; //x+y
 		int quotient = 0; // 몫
 		int result =0; // 최종 결과
+		String character = "0123456789abcdef"; //11진수부터 16진수를 위한 문자
+		char cha1 ='a'; //
+		char cha2 = 'b';
+		char [] cha = new char [10];
+		int j = 0;
 		
 		Scanner scanner = new Scanner(System.in);
 		
@@ -22,20 +27,66 @@ public class NumeralSystem {
 		y = scanner.nextInt(m);
 		System.out.println("k를 입력하시오 : ");
 		k = scanner.nextInt();
-		
-		sum = x+y;
-		while(sum>k){
-			quotient = sum/k; // 142/9 = 15, 15/9 =
-			remainder = sum%k; // 142%9 = 7, 15%9 = 6
-			sum = remainder;
-			remainder = sum*i; // 7*i = 1, 6*10 = 60
-			i = i*10; // i = 10, i = 100
-			result = result + remainder; //result = 7 , 67
-			sum = quotient; // remainder = 15 , 1 = sum
+		if(n<17 && n>1 && m<17 && m>1 && k<17 && k>1 ) {
+			sum = x+y;
+			while(sum>=k){
+				quotient = sum/k; // 2110/14 = 150, 150/14 = 10
+				remainder = sum%k; // 2110%14 = 10, 150%14 = 10  
+				if( k < 17 && k > 9) {
+					if(remainder>9 && remainder<k) {
+						while(quotient>=10) {
+							cha[j] = character.charAt(remainder); // cha[0] = 'a'
+							j++;
+							sum = quotient; // sum = 10
+							break;
+						}
+						if(quotient>k) {
+							continue;
+						}
+						else break;
+					}else{
+						sum = quotient;
+						if(sum>k)continue;
+						else break;
+					}
+					
+				}
+				else{
+					sum = remainder;
+					remainder = sum*i; // 7*i = 1, 6*10 = 60
+					i = i*10; // i = 10, i = 100
+					result = result + remainder; //result = 7 , 67
+					sum = quotient; // remainder = 15 , 1 = sum
+				}
+			}
+			if( k < 17 && k > 9) {
+				if(quotient>9 && quotient<k) {
+					cha[j] = character.charAt(quotient); 
+					System.out.print(cha[j]);
+					for(i = 0; i < j; i++) {
+						System.out.print(cha[i]);
+					}
+					if(remainder<10) System.out.println(remainder);
+					/*
+					cha2 = character.charAt(quotient);
+					System.out.print(cha2);
+					if(remainder<10) {
+						System.out.println(remainder);
+					}
+					else if(remainder>9 && remainder <17) {
+						System.out.println(cha1);
+					}
+					*/
+				}
+			}
+			else{
+				quotient = quotient*i; // 1 * 100
+				result = result + quotient; // 67 + 100
+				System.out.println("x + y : " +result); // 167
+			}
 		}
-		quotient = quotient*i; // 1 * 100
-		result = result + quotient; // 67 + 100
-		System.out.println("x + y : " +result); // 167
+		else {
+			System.out.println("진수를 잘못 선택하였습니다.");
+		}
 	}
-
 }
